f8a3a9146d6189d90b503bcdd62e5718df8f7bba
Jason Kai <jason.kai@childmind.org>
2024-07-18 17:07:58 -0400

    Update CLI parser

--- src/bidsapp_helper/parser.py
+++ src/bidsapp_helper/parser.py
@@ -1,50 +1,92 @@
 """Parser-creator."""
 
 import pathlib as pl
-from argparse import ArgumentParser, Namespace
-from typing import Sequence
+from argparse import ArgumentParser, FileType
+from collections.abc import Sequence
+from typing import Any, Callable, overload
+
+import yaml
 
 
-class BidsAppArgumentParser:
-    """BIDS-app CLI parser."""
+class BidsAppArgumentParser(ArgumentParser):
+    """Representation of BIDS app CLI."""
 
-    def __init__(self, app_name: str, description: str) -> None:  # noqa: D107
-        self.parser = ArgumentParser(
+    def __init__(self, app_name: str, description: str, *args, **kwargs) -> None:
+        super().__init__(  # type: ignore
             prog=app_name,
             usage="%(prog)s bids_dir output_dir analysis_level [options]",
-            description=f"""
-            {description}
-            """,
+            description=description,
+            *args,
+            **kwargs,
         )
         self._add_common_args()
 
     def _add_common_args(self) -> None:
-        """Common bids arguments."""
-        self.parser.add_argument(
-            "bids_dir", action="store", type=pl.Path, help="path to BIDS dataset"
-        )
-
-        self.parser.add_argument(
-            "output_dir",
+        self.add_argument(
+            "bids_dir",
             action="store",
             type=pl.Path,
-            help="path to output directory",
+            help="dataset directory (bids + derivatives)",
         )
-        self.parser.add_argument(
+        self.add_argument(
+            "output_dir", action="store", type=pl.Path, help="output directory"
+        )
+        self.add_argument(
             "analysis_level",
             metavar="analysis_level",
             type=str,
             choices=["participant"],  # Initial choices
-            help="analysis level to be performed",
+            help="{%(choices)s}",
         )
 
-    def update_analysis_level(self, choices: list[str]) -> None:
-        """Helper to update the choices available."""
-        for action in self.parser._actions:
+    def _get_arg_type(self, key: str) -> Callable[[str], Any] | FileType | None:
+        """Retrieve argument type based on its key."""
+        for action in self._actions:
+            if action.dest == key:
+                return action.type
+        raise KeyError("Unable to find configuration key")
+
+    def generate_config(self) -> dict[str, Any]:
+        """Generate config dict."""
+        config = {}
+        for action in self._actions:
+            if action.dest not in {"help", "version"}:
+                try:
+                    config[action.dest] = action.type(action.default)  # type: ignore
+                except Exception:
+                    config[action.dest] = action.default
+        return config
+
+    def load_config(self, config: dict[str, Any], config_fpath: pl.Path) -> None:
+        """Load arguments from configuration file."""
+        if (config_fpath := pl.Path(config_fpath)).suffix not in [".yaml", ".yml"]:
+            raise ValueError("Please provide a YAML configuration file")
+
+        if config_fpath.exists():
+            with open(config_fpath, "r") as config_file:
+                updated_attrs = yaml.safe_load(config_file)
+
+            for key, val in updated_attrs.items():
+                config[key] = self._get_arg_type(key)(val)  # type: ignore
+
+    @overload  # type: ignore
+    def parse_args(self, *args, config: dict[str, Any], **kwargs) -> None: ...
+
+    @overload
+    def parse_args(self, *args, config: None, **kwargs) -> dict[str, Any]: ...
+
+    def parse_args(self, *args, config: dict[str, Any] | None, **kwargs):
+        """Parse arguments into config dict."""
+        args = vars(super().parse_args(*args, **kwargs))
+        if config:
+            for key, val in args.items():  # type: ignore
+                config[key] = val
+        else:
+            return args
+
+    def update_analysis_level(self, choices: Sequence[str]) -> None:
+        """Update analysis-level choices."""
+        for action in self._actions:
             if action.dest == "analysis_level":
                 action.choices = choices
                 return
-
-    def parse_args(self, args: Sequence[str] | None = None) -> Namespace:
-        """Parse CLI arguments."""
-        return self.parser.parse_args(args)
